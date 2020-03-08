c64783bdd82fa915bb5dc1ae7dc950771898682e
Lok Sang Ho <sangstar@gmail.com>
2020-03-08 20:53:53 +1100

    play against computer

--- js/main.js
+++ js/main.js
@@ -10,25 +10,60 @@ const displayController = (() => {
     let player1;
     let player2;
     let currentPlayer;
+    let aiTrue;
 
-    const initialiseGame = () => {
-        let player1_val = document.getElementById("player_1_name_text").value;
-        let player2_val = document.getElementById("player_2_name_text").value;
-        
-        player1 = Player("X", player1_val != "" ? player1_val : "Player 1 ('X')" );
-        player2 = Player("0", player2_val != "" ? player2_val : "Player 2 ('0')");
-        document.getElementById("player_1_name_text").style.display = "none";
-        document.getElementById("player_2_name_text").style.display = "none";
-        document.getElementById("player1_name_display").textContent = player1_val != "" ? player1_val : "[Did not enter]";
-        document.getElementById("player2_name_display").textContent = player2_val != "" ? player2_val : "[Did not enter]";
-        document.getElementById("player1_name_display").style.display = "inline-block";
-        document.getElementById("player2_name_display").style.display = "inline-block";
-        currentPlayer = player1;
-        showGameMessage(currentPlayer.name + "'s turn")
-        gameBoard.startGame();
+    const initialiseGame = (aiTrue) => {
+
+
+
+        this.aiTrue = aiTrue;
+        if (!aiTrue) {
+            let player1_val = document.getElementById("player_1_name_text").value;
+            let player2_val = document.getElementById("player_2_name_text").value;
+
+            player1 = Player("X", player1_val != "" ? player1_val : "Player 1 ('X')");
+            player2 = Player("0", player2_val != "" ? player2_val : "Player 2 ('0')");
+            document.getElementById("player_1_name_text").style.display = "none";
+            document.getElementById("player_2_name_text").style.display = "none";
+            document.getElementById("player1_name_display").textContent = player1_val != "" ? player1_val : "[Did not enter]";
+            document.getElementById("player2_name_display").textContent = player2_val != "" ? player2_val : "[Did not enter]";
+            document.getElementById("player1_name_display").style.display = "inline-block";
+            document.getElementById("player2_name_display").style.display = "inline-block";
+            currentPlayer = getRandomPlayer();
+            showGameMessage(currentPlayer.name + "'s turn");
+            gameBoard.startGame();
+        } else {
+
+            let player1_val = document.getElementById("player_1_name_text").value;
+
+            player1 = Player("X", player1_val != "" ? player1_val : "Player 1 ('X')");
+            player2 = Player("0", null);
+            document.getElementById("player_1_name_text").style.display = "none";
+            document.getElementById("player_2_name_text").style.display = "none";
+            document.getElementById("player1_name_display").textContent = player1_val != "" ? player1_val : "[Did not enter]";
+            document.getElementById("player2_name_display").textContent = "Computer";
+            document.getElementById("player1_name_display").style.display = "inline-block";
+            document.getElementById("player2_name_display").style.display = "inline-block";
+            currentPlayer = getRandomPlayer();
+
+            gameBoard.startGame();
+            if (currentPlayer.name == null) {
+                console.log("here");
+                gameBoard.aiComputeMove(getComputerPlayerIfAIGame().symbol);
+            }
+        }
     }
 
+    const getRandomPlayer = () => (Math.floor(Math.random() * 2) == 0 ? player1 : player2);
+
+    const getAiTrue = () => aiTrue;
+
     const getCurrentPlayer = () => currentPlayer;
+
+    const getHumanPlayerIfAIGame = () => player1;
+
+    const getComputerPlayerIfAIGame = () => player2;
+
 
     const switchPlayer = () => {
         if (currentPlayer == player1) {
@@ -45,6 +80,9 @@ const displayController = (() => {
         getCurrentPlayer,
         initialiseGame,
         switchPlayer,
+        getAiTrue,
+        getHumanPlayerIfAIGame,
+        getComputerPlayerIfAIGame,
     }
 
 })();
@@ -77,10 +115,10 @@ const gameBoard = (() => {
     }
 
     const checkWinLoseOrDraw = () => {
-        if (checkWin("X")) {
+        if (checkWin("X", board_array)) {
             return "X";
         }
-        if (checkWin("0")) {
+        if (checkWin("0", board_array)) {
             return "0";
         }
         if (!board_array.some(function (symbol) {
@@ -91,7 +129,7 @@ const gameBoard = (() => {
         return null;
     }
 
-    const checkWin = (symbol) => {
+    const checkWin = (symbol, board_array) => {
         for (let i = 0; i < board_array.length; i++) {
             //check first row
             if (board_array[0] == symbol) {
@@ -146,30 +184,158 @@ const gameBoard = (() => {
 
     };
 
+    const isMovesLeft = (board_array) => {
+        return board_array.some(cell => cell == "");
+    }
+
+    const evaluate = (board_array) => {
+        // Checking for Rows for X or 0 victory
+        if (checkWin("X", board_array)) {
+            if (displayController.getHumanPlayerIfAIGame().symbol == "X") {
+                return -10;
+            } else {
+                return 10;
+            }
+        }
+        if (checkWin("0", board_array)) {
+            if (displayController.getHumanPlayerIfAIGame().symbol == "0") {
+                return -10;
+            } else {
+                return 10;
+            }
+        }
+        return 0;
+    }
+
+    const minimax = (board_array, depth, isMax) => {
+        let score = evaluate(board_array);
+
+        if (score == 10)
+            return score;
+
+        if (score == -10)
+            return score;
+
+        if (isMovesLeft(board_array) == false)
+            return 0;
+
+        if (isMax) {
+            let best = -1000;
+
+            for (let i = 0; i < board_array.length; i++) {
+                if (board_array[i] == "") {
+                    board_array[i] = displayController.getComputerPlayerIfAIGame().symbol;
+
+                    best = Math.max(best, minimax(board_array, depth + 1, !isMax));
+                    board_array[i] = "";
+                }
+            }
+
+
+
+            return best;
+        } else {
+            let best = 1000;
+            for (let i = 0; i < board_array.length; i++) {
+                if (board_array[i] == "") {
+                    board_array[i] = displayController.getHumanPlayerIfAIGame().symbol;
+
+                    best = Math.min(best, minimax(board_array, depth + 1, !isMax));
+                    board_array[i] = "";
+
+                }
+            }
+            return best;
+
+
+        }
+    }
+
+    const findBestMove = (board_array) => {
+        let bestVal = -1000;
+        let bestMove = -1;
+
+        for (let i = 0; i < board_array.length; i++) {
+            if (board_array[i] == "") {
+                board_array[i] = displayController.getComputerPlayerIfAIGame().symbol;
+
+                let moveVal = minimax(board_array, 0, false);
+                board_array[i] = "";
+                if (moveVal > bestVal) {
+                    bestMove = i;
+                    bestVal = moveVal;
+                }
+            }
+        }
+        return bestMove;
+
+    }
+
+    const aiComputeMove = (symbol) => {
+        //    let testIndex;
+        //    do {
+        //        testIndex = Math.floor(Math.random() * 9)
+        //    } while (board_array[testIndex] != "");
+
+        //    board_array[testIndex] = symbol;
+
+        let board_array_copy = [...board_array];
+
+        board_array[findBestMove(board_array_copy)] = symbol;
+
+
+        displayCorrectMessage();
+        if (!gameEnd) {
+            displayController.switchPlayer();
+            let playerName = displayController.getCurrentPlayer().name;
+            displayController.showGameMessage((playerName == null ? "Computer" : playerName) + "'s turn");
+            render();
+        }
+    };
+
+    const displayCorrectMessage = () => {
+        let result = checkWinLoseOrDraw(board_array);
+        let playerName = displayController.getCurrentPlayer().name;
+        if (result == "X" || result == "0") {
+            displayController.showGameMessage((playerName == null ? "Computer" : playerName) + " wins!");
+            gameEnd = true;
+        } else if (result == "D") {
+            displayController.showGameMessage("It's a Draw!");
+            gameEnd = true;
+        } else {
+
+        }
+        render();
+    }
+
     const setupListener = () => {
         document.addEventListener("click", function (e) {
             if (!gameEnd && e.target.classList[0] == "game_board_cell") {
                 let array_index = e.target.getAttribute("data-cell-num")
                 if (board_array[array_index] == "") {
                     board_array[array_index] = displayController.getCurrentPlayer().symbol;
-                    let result = checkWinLoseOrDraw();
-                    if (result == "X" || result == "0") {
-                        displayController.showGameMessage(displayController.getCurrentPlayer().name + " wins!");
-                        gameEnd = true;
-                    } else if (result == "D") {
-                        displayController.showGameMessage("It's a Draw!");
 
-                        gameEnd = true;
-                    } else {
+                    displayCorrectMessage();
+                    if (!gameEnd) {
                         displayController.switchPlayer();
-                        displayController.showGameMessage(displayController.getCurrentPlayer().name + "'s turn");
+                        if (aiTrue && displayController.getCurrentPlayer().name == null) {
+                            // computer's turn
+                            aiComputeMove(displayController.getCurrentPlayer().symbol);
+                        } else {
+                            let playerName = displayController.getCurrentPlayer().name;
+                            displayController.showGameMessage((playerName == null ? "Computer" : playerName) + "'s turn");
+                            render();
+                        }
                     }
-                    render();
 
                 }
             }
             if (e.target.id == "start_game") {
-                displayController.initialiseGame();
+                displayController.initialiseGame(false);
+
+            }
+            if (e.target.id == "play_ai") {
+                displayController.initialiseGame(true);
 
             }
 
@@ -181,6 +347,7 @@ const gameBoard = (() => {
         startGame,
         setupListener,
         checkWinLoseOrDraw,
+        aiComputeMove,
     };
 
 })();
